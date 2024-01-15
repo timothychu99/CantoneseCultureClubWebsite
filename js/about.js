6b13bad06fea0a272e13cc7a4247cabeccd149ee
timothychu99 <timothychu99@gmail.com>
2024-01-14 23:41:10 -0800

    added linktree and footer center

--- js/about.js
+++ js/about.js
@@ -1,12 +1,8 @@
 // Add your JavaScript code here
-function moveToSection(id) {
-    var element = document.getElementById(id);
-    var headerOffset = 130;
-    var elementPosition = element.getBoundingClientRect().top;
-    var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
-    
-    window.scrollTo({
-        top: offsetPosition,
-        behavior: "smooth"
-    });   
-}
+function ani() {
+        document.getElementById('down_arrow').className = 'image_trunc';
+} 
+
+
+
+
